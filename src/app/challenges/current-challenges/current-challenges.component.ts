8449edd1d74d95a8c7bb52b0994aca87226a93e6
Gagulo <grzegorz.sokalski@hotmail.com>
2019-02-15 13:30:02 +0100

    creating first elements

---
+++ src/app/challenges/current-challenges/current-challenges.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ns-current-challenge',
+    templateUrl: './current-challenges.component.html',
+    styleUrls: ['./current-challenges.component.css'],
+    moduleId: module.id
+})
+export class CurrentChallengeComponent {
+    challengeDescription = '';
+    currentChallenge = '';
+
+    onSetChallenge() {
+        this.currentChallenge = this.challengeDescription;
+    };
+}
