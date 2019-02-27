67f2ab9c3e5078af702a4b356f92154e92d4dea2
Gagulo <grzegorz.sokalski@hotmail.com>
2019-02-27 16:29:34 +0100

    lazy loading edit component

--- src/app/challenges/challenges.module.ts
+++ src/app/challenges/challenges.module.ts
@@ -2,7 +2,6 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule } from 'nativescript-angular/common';
 
 import { ChallengesRoutingModule } from './challenges-routing.module';
-import { ChallengeEditComponent } from './challenge-edit/challenge-edit.component';
 import { ChallengeTabsComponent } from './challenge-tabs/challenge-tabs.component';
 import { CurrentChallengeComponent } from './current-challenge/current-challenge.component';
 import { TodayComponent } from './today/today.component';
@@ -10,7 +9,6 @@ import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    ChallengeEditComponent,
     ChallengeTabsComponent,
     CurrentChallengeComponent,
     TodayComponent
