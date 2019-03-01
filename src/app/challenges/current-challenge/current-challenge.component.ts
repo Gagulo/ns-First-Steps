17af03845047e69aa43b08c3afd61a5c00e219bc
Gagulo <grzegorz.sokalski@hotmail.com>
2019-03-01 15:02:12 +0100

    styling current challenge component

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -6,7 +6,7 @@ import { UIService } from '~/app/shared/ui.service';
 @Component({
   selector: 'ns-current-challenge',
   templateUrl: './current-challenge.component.html',
-  styleUrls: ['./current-challenge.component.common.css', './current-challenge.component.css'],
+  styleUrls: ['./_current-challenge.component.common.scss', './current-challenge.component.scss'],
   moduleId: module.id
 })
 export class CurrentChallengeComponent {
